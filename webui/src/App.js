298d5c3bccbe3d44a7c9ca2e771c62081b8bef7f
Lucas Rigato <lucas.rigato@db1.com.br>
2019-06-10 11:00:09 -0300

    Add time and update WS status

--- webui/src/App.js
+++ webui/src/App.js
@@ -5,15 +5,6 @@ import Navigation from './Navigation';
 
 class App extends Component {
   render() {
-/*
-    if(window.location.protocol !== "https:" && process.env.NODE_ENV !== 'development') {
-      return (
-        <div className="App">
-          <div className="insecure-scheme">The Web UI is not available over HTTP. To use it; enable SSL, restart GAD and browse to this page using HTTPS.</div>
-        </div>
-      );
-    }
-*/
     return (
       <div className="App">
         <Navigation />
